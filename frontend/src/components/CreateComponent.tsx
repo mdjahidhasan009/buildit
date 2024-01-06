f1973325aa1998164ff75a63d14821fa79950b8a
mdjahidhasan009 <jahid60012@gmail.com>
2024-01-06 20:46:11 +0600

    created all shapes

--- frontend/src/components/CreateComponent.tsx
+++ frontend/src/components/CreateComponent.tsx
@@ -49,6 +49,76 @@ const CreateComponent = ({ info, currentComponent, removeComponent }) => {
       </div>
   }
 
+  if(info.name === 'shape' && info.type === 'circle') {
+    html =
+      <div
+        id={randValue.toString()}
+        onClick={() => info.setCurrentComponent(info)}
+        style={{
+          left: info.left + 'px',
+          top: info.top + 'px',
+          zIndex: info.z_index,
+          transform: info.rotate ? `rotate(${info.rotate}deg)` : `rotate(0deg)`,
+        }}
+        className='absolute group hover:border-[2px] hover:border-indigo-500'
+      >
+        <div
+          id={`${randValue}c`}
+          className='rounded-full'
+          style={{
+            width: info.width + 'px',
+            height: info.width + 'px',
+            background: info.color,
+            opacity: info.opacity,
+          }}
+        >
+
+        </div>
+        {currentComponent.id === info.id &&
+            <div
+                onClick={() => removeComponent(info.id)}
+                className='px-3 py-2 bg-white absolute top-0 hidden group-hover:block cursor-pointer rounded-md'>
+              <BsTrash />
+            </div>
+        }
+      </div>
+  }
+
+  if(info.name === 'shape' && info.type === 'trangle') {
+    html =
+      <div
+        id={randValue.toString()}
+        onClick={() => info.setCurrentComponent(info)}
+        style={{
+          left: info.left + 'px',
+          top: info.top + 'px',
+          zIndex: info.z_index,
+          transform: info.rotate ? `rotate(${info.rotate}deg)` : `rotate(0deg)`,
+        }}
+        className='absolute group hover:border-[2px] hover:border-indigo-500'
+      >
+        <div
+          id={`${randValue}c`}
+          style={{
+            width: info.width + 'px',
+            height: info.height + 'px',
+            background: info.color,
+            opacity: info.opacity,
+            clipPath: 'polygon(50% 0, 100% 100%, 0 100%)'
+          }}
+        >
+
+        </div>
+        {currentComponent.id === info.id &&
+            <div
+                onClick={() => removeComponent(info.id)}
+                className='px-3 py-2 bg-white absolute top-0 hidden group-hover:block cursor-pointer rounded-md'>
+              <BsTrash />
+            </div>
+        }
+      </div>
+  }
+
   return html;
 }
 
