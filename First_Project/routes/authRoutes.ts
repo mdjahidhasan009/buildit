a0f1188f162e18bf6cebbdd0d77e559955bc458d
mdjahidhasan009 <jahid60012@gmail.com>
2024-02-03 04:47:13 +0600

    delete other project

--- First_Project/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authController = require('../controllers/authController');
-
-const router = express.Router();
-
-router.post('/register', authController.registerUser);
-router.post('/login', authController.loginUser);
-
-export default router;
