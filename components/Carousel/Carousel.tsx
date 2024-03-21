fafc87b6fa4a2d93548497cf9db4fbfd3cd7699a
mdjahidhasan009 <jahid60012@gmail.com>
2024-03-21 22:25:56 +0600

    Left arrow and infinite scrolling done

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -16,15 +16,15 @@ const CardSlider: React.FC<CardSliderProps> = ({ cards }) => {
   // const [startX, setStartX] = useState<number | null>(null);
   // const [startScrollLeft, setStartScrollLeft] = useState<number | null>(null);
   // const carouselRef = useRef<HTMLDivElement>(null);
-  // const firstCardWidth = useRef<number | null>(null);
+  // const firstCardWidthRef = useRef<number | null>(null);
   //
   // useEffect(() => {
   //   if (carouselRef.current) {
-  //     firstCardWidth.current = carouselRef.current.querySelector(".card")?.offsetWidth ?? 0;
-  //     console.log('firstCardWidth=', firstCardWidth.current)
+  //     firstCardWidthRef.current = carouselRef.current.querySelector(".card")?.offsetWidth ?? 0;
+  //     console.log('firstCardWidthRef=', firstCardWidthRef.current)
   //
-  //     if (firstCardWidth.current) {
-  //       const cardPerView = Math.round(carouselRef.current.offsetWidth / firstCardWidth.current) - 1;
+  //     if (firstCardWidthRef.current) {
+  //       const cardPerView = Math.round(carouselRef.current.offsetWidth / firstCardWidthRef.current) - 1;
   //       console.log('cardPerView='+ cardPerView)
   //       const carouselChildrens = [...carouselRef.current.children];
   //
@@ -73,7 +73,7 @@ const CardSlider: React.FC<CardSliderProps> = ({ cards }) => {
   //     setInterval(() => {
   //       if (!isDragging) {
   //         carouselRef.current?.scrollBy({
-  //           left: firstCardWidth.current ?? 0,
+  //           left: firstCardWidthRef.current ?? 0,
   //           behavior: 'smooth'
   //         });
   //       }
@@ -95,29 +95,34 @@ const CardSlider: React.FC<CardSliderProps> = ({ cards }) => {
   //   };
   // }, [cards.length, isDragging]);
 
+  const wrapperRef = useRef<HTMLDivElement>(null);
   // const [isDragging, setIsDragging] = useState<boolean>(false);
   const isDraggingRef = useRef(false);
   // const [startX, setStartX] = useState<number | null>(null);
   const startXRef = useRef<number | null>(null);
   // const [startScrollLeft, setStartScrollLeft] = useState<number | null>(null);
-  const startScrollLeftRef = useRef<number | null>(null);
+  const startScrollLeftRef = useRef<number | null>(0);
   const carouselRef = useRef<HTMLDivElement>(null);
-  const firstCardWidth = useRef<number | null>(null);
+  const firstCardWidthRef = useRef<number>(null);
+  const leftArrowRef = useRef<HTMLDivElement>(null); // Ref for left arrow
+  const rightArrowRef = useRef<HTMLDivElement>(null); // Ref for right arrow
+  const isAutoPlayRef = useRef<boolean>(true);
+  const timeoutIdRef = useRef<number>(0);
 
   useEffect(() => {
     if (carouselRef.current) {
-      firstCardWidth.current = carouselRef.current.querySelector(".card")?.offsetWidth ?? 0;
-      console.log('firstCardWidth=', firstCardWidth.current)
+      const firstCardElement = Array.from(carouselRef.current.children).find(child => child.classList.contains('card'));
+      if (firstCardElement) {
+        firstCardWidthRef.current = firstCardElement.getBoundingClientRect().width;
+      }
+      console.log('firstCardWidthRef=', firstCardWidthRef.current)
 
-      if (firstCardWidth.current) {
-        const cardPerView = Math.round(carouselRef.current.offsetWidth / firstCardWidth.current) ;
+      if (firstCardWidthRef.current) {
+        const cardPerView = Math.round(carouselRef.current.offsetWidth / firstCardWidthRef.current) ;
         console.log('cardPerView='+ cardPerView)
+
+
         const carouselChildrens = [...carouselRef.current.children];
-
-        carouselRef.current.classList.add("no-transition");
-        carouselRef.current.scrollLeft = carouselRef.current.offsetWidth;
-        carouselRef.current.classList.remove("no-transition");
-
         carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
           carouselRef.current?.insertAdjacentHTML("afterbegin", card.outerHTML);
         });
@@ -126,15 +131,30 @@ const CardSlider: React.FC<CardSliderProps> = ({ cards }) => {
           carouselRef.current?.insertAdjacentHTML("beforeend", card.outerHTML);
         });
 
+        carouselRef.current.classList.add("no-transition");
+        carouselRef.current.scrollLeft = carouselRef.current.offsetWidth;
+        carouselRef.current.classList.remove("no-transition");
+
+
+
+        leftArrowRef.current?.addEventListener("click", () => handleArrowClick('left'));
+        rightArrowRef.current?.addEventListener("click", () => handleArrowClick('right'));
+
         carouselRef.current.scrollLeft = carouselRef.current.offsetWidth;
       }
     }
 
+    const handleArrowClick = (direction: 'left' | 'right') => {
+      if (carouselRef.current) {
+        carouselRef.current.scrollLeft += direction === 'left' ? -firstCardWidthRef.current! : firstCardWidthRef.current!;
+      }
+    };
+
     const dragStart = (e: MouseEvent) => {
       isDraggingRef.current = true;
-      startXRef.current = e.pageX;
-      startScrollLeftRef.current  = carouselRef.current?.scrollLeft ?? null;
       carouselRef.current?.classList.add("dragging");
+      startXRef.current = e.pageX;
+      startScrollLeftRef.current  = carouselRef.current?.scrollLeft ?? 0;
     };
 
     const dragging = (e: MouseEvent) => {
@@ -149,54 +169,66 @@ const CardSlider: React.FC<CardSliderProps> = ({ cards }) => {
 
     const infiniteScroll = () => {
       if (carouselRef.current?.scrollLeft === 0) {
+        carouselRef.current.classList.add("no-transition");
         carouselRef.current.scrollLeft = (carouselRef.current.scrollWidth ?? 0) - (2 * (carouselRef.current.offsetWidth ?? 0));
-      } else if (Math.ceil(carouselRef.current?.scrollLeft ?? 0) === (carouselRef.current?.scrollWidth ?? 0) - (carouselRef.current?.offsetWidth ?? 0)) {
+        carouselRef.current.classList.remove("no-transition");
+      } else if (Math.ceil(carouselRef.current?.scrollLeft ?? 0) === ((carouselRef.current?.scrollWidth ?? 0) - (carouselRef.current?.offsetWidth ?? 0))) {
+        carouselRef.current.classList.add("no-transition");
         carouselRef.current.scrollLeft = carouselRef.current.offsetWidth ?? 0;
+        carouselRef.current.classList.remove("no-transition");
       }
+
+      // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+      clearTimeout(timeoutIdRef.current);
+      if(!wrapperRef.current.matches(":hover")) autoPlay();
     };
 
     const autoPlay = () => {
-      setInterval(() => {
-        if (!isDraggingRef.current) {
-          carouselRef.current?.scrollBy({
-            left: firstCardWidth.current ?? 0,
-            behavior: 'smooth'
-          });
-        }
-      }, 2500);
+      // setInterval(() => {
+      //   if (!isDraggingRef.current) {
+      //     carouselRef.current?.scrollBy({
+      //       left: firstCardWidthRef.current ?? 0,
+      //       behavior: 'smooth'
+      //     });
+      //   }
+      // }, 2500);
+      if(window.innerWidth < 800 || !isAutoPlayRef.current) return; // Return if window is smaller than 800 or isAutoPlay is false
+      // Autoplay the carousel after every 2500 ms
+      timeoutIdRef.current = setTimeout(() => carouselRef.current.scrollLeft += firstCardWidthRef.current, 2500);
     };
 
-    // autoPlay();
+    autoPlay();
 
     carouselRef.current?.addEventListener("mousedown", dragStart);
     carouselRef.current?.addEventListener("mousemove", dragging);
     document.addEventListener("mouseup", dragStop);
-    // carouselRef.current?.addEventListener("scroll", infiniteScroll);
+    carouselRef.current?.addEventListener("scroll", infiniteScroll);
 
     return () => {
       carouselRef.current?.removeEventListener("mousedown", dragStart);
       carouselRef.current?.removeEventListener("mousemove", dragging);
       document.removeEventListener("mouseup", dragStop);
-      // carouselRef.current?.removeEventListener("scroll", infiniteScroll);
+      carouselRef.current?.removeEventListener("scroll", infiniteScroll);
+      wrapperRef.current.addEventListener("mouseenter", () => clearTimeout(timeoutIdRef.current));
+      wrapperRef.current.addEventListener("mouseleave", autoPlay);
+      leftArrowRef.current?.removeEventListener("click", () => handleArrowClick('left'));
+      rightArrowRef.current?.removeEventListener("click", () => handleArrowClick('right'));
     };
   }, [cards.length, isDraggingRef.current]);
 
   return (
-    <div className="wrapper mx-auto bg-white rounded-lg overflow-hidden">
-      <i id="left" className="fa-solid fa-angle-left"></i>
+    <div ref={wrapperRef} className="wrapper mx-auto bg-white rounded-lg overflow-hidden">
+      <i id="left" ref={leftArrowRef} className="fa-solid fa-angle-left"></i>
       <div ref={carouselRef} className="carousel flex overflow-x-auto">
-        {console.log(cards)}
         {cards.map((card, index) => (
           <div key={index} className="card flex-shrink-0 mr-4 border-4 border-red-950">
             <div className="img">
               <img className="w-[250px] h-[250px]" src={card?.imageUrl} alt="img" draggable={false}/>
             </div>
-            {console.log(card)}
-            {/*<h2 className="text-lg font-semibold">{card.name}</h2>*/}
-            {/*<span className="text-sm text-gray-600">{card.role}</span>*/}
           </div>
         ))}
       </div>
+      <i id="right" ref={rightArrowRef} className="fa-solid fa-angle-right"></i>
     </div>
   );
 };
