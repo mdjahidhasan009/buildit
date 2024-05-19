7b826f9ddbead48ca7346d6e8ff3465f02cd67c3
mdjahidhasan009 <jahid60012@gmail.com>
2024-05-19 18:39:09 +0600

    [fixed] design sidenav height

--- app/(design)/components/SideBar.tsx
+++ app/(design)/components/SideBar.tsx
@@ -19,52 +19,52 @@ const SideBar = () => {
 
 
   return (
-    <div className={`w-[80px] bg-[#18191B] z-50 text-gray-400 overflow-y-auto`}>
+    <div className={`w-[80px] bg-[#18191B] z-50 text-gray-400 overflow-y-auto flex flex-col items-center justify-center`}>
       <div
         onClick={() => handleOnClick('design', 'design')}
-        className={`${selectedSidebarItemName === 'design' ? 'bg-[#252627]' : ''} w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
+        className={`${selectedSidebarItemName === 'design' ? 'bg-[#252627]' : ''} w-full h-[75px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
         <span className='text-2xl'><BsGrid1X2/></span>
         <span className='text-xs'>Design</span>
       </div>
 
       <div
         onClick={() => handleOnClick('shape', 'shape')}
-        className={`${selectedSidebarItemName === 'shape' ? 'bg-[#252627]' : ''} w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
+        className={`${selectedSidebarItemName === 'shape' ? 'bg-[#252627]' : ''} w-full h-[75px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
         <span className='text-2xl'><FaShapes/></span>
         <span className='text-xs'>Shapes</span>
       </div>
 
       <div
         onClick={() => handleOnClick('image', 'uploadImage')}
-        className={`${selectedSidebarItemName === 'image' ? 'bg-[#252627]' : ''} w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
+        className={`${selectedSidebarItemName === 'image' ? 'bg-[#252627]' : ''} w-full h-[75px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
         <span className='text-2xl'><GoCloud/></span>
         <span className='text-xs'>Upload</span>
       </div>
 
       <div
         onClick={() => handleOnClick('text', 'text')}
-        className={`${selectedSidebarItemName === 'text' ? 'bg-[#252627]' : ''} w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
+        className={`${selectedSidebarItemName === 'text' ? 'bg-[#252627]' : ''} w-full h-[75px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
         <span className='text-2xl'><TfiText/></span>
         <span className='text-xs'>Text</span>
       </div>
 
       <div
         onClick={() => handleOnClick('project', 'projects')}
-        className={`${selectedSidebarItemName === 'project' ? 'bg-[#252627]' : ''} w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
+        className={`${selectedSidebarItemName === 'project' ? 'bg-[#252627]' : ''} w-full h-[75px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
         <span className='text-2xl'><FaFolder/></span>
         <span className='text-xs'>Project</span>
       </div>
 
       <div
         onClick={() => handleOnClick('initImage', 'images')}
-        className={`${selectedSidebarItemName === 'initImage' ? 'bg-[#252627]' : ''} w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
+        className={`${selectedSidebarItemName === 'initImage' ? 'bg-[#252627]' : ''} w-full h-[75px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
         <span className='text-2xl'><BsFillImageFill/></span>
         <span className='text-xs'>Images</span>
       </div>
 
       <div
         onClick={() => handleOnClick('background', 'background')}
-        className={`${selectedSidebarItemName === 'background' ? 'bg-[#252627]' : ''} w-full h-[80px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
+        className={`${selectedSidebarItemName === 'background' ? 'bg-[#252627]' : ''} w-full h-[75px] cursor-pointer flex justify-center flex-col items-center gap-1 hover:text-gray-100`}>
         <span className='text-2xl'><RxTransparencyGrid/></span>
         <span className='text-xs'>Background</span>
       </div>
