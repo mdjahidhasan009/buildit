00daa6f054e0c23a2ca959fa259b1f705f582824
mdjahidhasan009 <jahid60012@gmail.com>
2024-06-19 22:30:17 +0600

    [fixing] build

--- utils/prismaClient.ts
+++ utils/prismaClient.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { Pool } from "@neondatabase/serverless";
-
-const neon = new Pool({
-    connectionString: process.env.DIRECT_DATABASE_URL,
-});
-
-const adapter = new PrismaNeon(neon);
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaNeon } from "@prisma/adapter-neon";
+// import { Pool } from "@neondatabase/serverless";
+//
+// const neon = new Pool({
+//     connectionString: process.env.DIRECT_DATABASE_URL,
+// });
+//
+// const adapter = new PrismaNeon(neon);
+// const prisma = new PrismaClient({ adapter });
+//
+// export { prisma };
