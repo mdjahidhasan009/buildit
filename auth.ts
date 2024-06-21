ebf3e742c97064b06f37b370652e2ec8be63d752
mdjahidhasan009 <jahid60012@gmail.com>
2024-06-22 04:52:24 +0600

    [modifed] session

--- auth.ts
+++ auth.ts
@@ -1,15 +1,47 @@
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import prisma from './prisma/prisma';
-import GitHub from "next-auth/providers/github";
+import Github from 'next-auth/providers/github';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+  session: { strategy: 'jwt' },
   adapter: PrismaAdapter(prisma),
+  providers: [Github],
   callbacks: {
-    async session({ session, user }: any) {
-      session.user.id = user.id;
-      return session;
+    // authorized({ auth, request: { nextUrl } }) {
+    //   const isLoggedIn = !!auth?.user;
+    //   const paths = ['/profile', '/client-side'];
+    //   const isProtected = paths.some((path) =>
+    //     nextUrl.pathname.startsWith(path)
+    //   );
+    //
+    //   if (isProtected && !isLoggedIn) {
+    //     const redirectUrl = new URL('/api/auth/signin', nextUrl.origin);
+    //     redirectUrl.searchParams.append('callbackUrl', nextUrl.href);
+    //     return Response.redirect(redirectUrl);
+    //   }
+    //   return true;
+    // },
+    jwt: ({ token, user }) => {
+      if (user) {
+        const u = user as unknown as any;
+        return {
+          ...token,
+          id: u.id,
+          randomKey: u.randomKey,
+        };
+      }
+      return token;
+    },
+    session(params) {
+      return {
+        ...params.session,
+        user: {
+          ...params.session.user,
+          id: params.token.id as string,
+          randomKey: params.token.randomKey,
+        },
+      };
     },
   },
 });
